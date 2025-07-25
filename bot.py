41e5e7088ab4f365d7517d7f896db6cde56f35d7
Maksud <servanramazanov274@gmail.com>
2025-07-26 01:32:11 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,3 @@
+API_ID = 842677    # sənin my.telegram.org saytından aldığın API ID
+API_HASH = "a1b2c3d4e5f6g7h8i9j0"  # sənin API HASH (uzun simvol ardıcıllığı)
+BOT_TOKEN = "8426776764:AAH5LFdu4ZHgvx9qVFNP_wqrzc6gSMsv5Z4"  
